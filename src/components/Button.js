a8ae52b8882aa10cdfa33c08f14714008e874eed
Juakata <ha.uzquiano@hotmail.com>
2020-02-04 14:32:20 -0600

    Add styling to the buttons and fix issue with unique key

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { name } = props;
+  const { object } = props;
+  const className = object.id % 4 === 0 || object.id === 19 ? 'operator' : 'normal';
+  if (object.name === '0') {
+    return (
+      <button type="button" id="zero">{object.name}</button>
+    );
+  }
   return (
-    <button type="button">{name}</button>
+    <button type="button" className={className}>{object.name}</button>
   );
 };
 
 Button.propTypes = {
-  name: PropTypes.string.isRequired,
+  object: PropTypes.object,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,17 +5,17 @@ export default class ButtonPanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      names: ['AC', '+/-', '%', '+',
-        '7', '8', '9', 'X',
-        '4', '5', '6', '-',
-        '1', '2', '3', '+',
-        '0', '.', '='],
+      objects: [{ id: 1, name: 'AC' }, { id: 2, name: '+/-' }, { id: 3, name: '%' },
+        { id: 4, name: '÷' }, { id: 5, name: '7' }, { id: 6, name: '8' }, { id: 7, name: '9' },
+        { id: 8, name: 'x' }, { id: 9, name: '4' }, { id: 10, name: '5' }, { id: 11, name: '6' },
+        { id: 12, name: '-' }, { id: 13, name: '1' }, { id: 14, name: '2' }, { id: 15, name: '3' },
+        { id: 16, name: '+' }, { id: 17, name: '0' }, { id: 18, name: '.' }, { id: 19, name: '=' }],
     };
   }
 
   render() {
-    const { names } = this.state;
-    const buttons = names.map((key, name) => <Button name={name} key={key} />);
+    const { objects } = this.state;
+    const buttons = objects.map(object => <Button object={object} key={object.id} />);
 
     return (
       <div className="grid-4">{buttons}</div>
